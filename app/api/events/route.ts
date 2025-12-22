b9c964bfc52990d94f80ec19b00ced20698dcc63
Sathwik <sathwikpilla2006@gmail.com>
2025-12-22 22:41:56 +0530

    Add API route

---
+++ app/api/events/route.ts
@@ -0,0 +1,27 @@
+import {NextRequest, NextResponse} from "next/server";
+import {isInstanceOf} from "@posthog/core";
+import connectToDatabase from "@/lib/mongodb";
+import Event from '@database/event.model'
+
+export async function POST(req:NextRequest){
+    try {
+        await connectToDatabase();
+
+        const formData = await req.formData(); //in express.js it's req.body();
+
+        let event;
+
+        try {
+            event = Object.fromEntries(formData.entries());
+        } catch (e) {
+return NextResponse.json({message: 'Invalid JSON data format', {status: 400})
+        }
+
+        const createdEvent = await Event.create(event);
+
+            return NextResponse.json({message: 'Event created successfully', event: createdEvent}, {status: 201});
+    } catch(e) {
+        console.error(e);
+        return NextResponse.json({message:'Event Creation Failed', error: e instanceOf Error ? e.message : 'Unknown'}, {status: 500});
+    }
+}
